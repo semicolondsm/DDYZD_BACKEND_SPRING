47c985fa04535cea6bb6601be0e3f08ba05d9d84
leeseojune <sung07288346@gmail.com>
2021-01-17 22:51:25 +0900

    UPDATE; FeedServiceImpl

--- src/main/java/com/semicolon/spring/service/feed/FeedServiceImpl.java
+++ src/main/java/com/semicolon/spring/service/feed/FeedServiceImpl.java
@@ -1,10 +1,19 @@
 package com.semicolon.spring.service.feed;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
+@Service
 public class FeedServiceImpl implements FeedService{
     @Override
     public void fileUpload(MultipartFile file) {
-
+        try{
+            file.transferTo(new File("C:/Semicolon/DDYZD_V2_BACKEND_SPRING/"+file.getOriginalFilename()));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
