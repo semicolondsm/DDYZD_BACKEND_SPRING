4bc3eb84dfefafd61229162dfa99d9fd50b992b7
leeseojune <sung07288346@gmail.com>
2021-01-17 22:51:41 +0900

    UPDATE; FeedController

--- src/main/java/com/semicolon/spring/controller/FeedController.java
+++ src/main/java/com/semicolon/spring/controller/FeedController.java
@@ -1,9 +1,19 @@
 package com.semicolon.spring.controller;
 
+import com.semicolon.spring.service.feed.FeedService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 public class FeedController {
+    private final FeedService feedService;
+
+    @PostMapping("/test")
+    public void fileUpload(@RequestParam("file")MultipartFile file){
+        feedService.fileUpload(file);
+    }
 }
