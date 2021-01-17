0d123a20076b71c9a736d4f014a53aacb394506d
leeseojune <sung07288346@gmail.com>
2021-01-17 23:35:03 +0900

    UPDATE; FeedService

--- src/main/java/com/semicolon/spring/service/feed/FeedService.java
+++ src/main/java/com/semicolon/spring/service/feed/FeedService.java
@@ -1,9 +1,11 @@
 package com.semicolon.spring.service.feed;
 
+import com.semicolon.spring.dto.FeedDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface FeedService {
     void fileUpload(MultipartFile file);
+    FeedDTO.writeFeedResponse writeFeed(FeedDTO.writeFeed request);
 }
