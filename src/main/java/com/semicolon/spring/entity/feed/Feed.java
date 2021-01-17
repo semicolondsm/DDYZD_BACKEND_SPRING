094f3f8f860002a04ac362b7e6aa4371db24bc98
leeseojune <sung07288346@gmail.com>
2021-01-17 20:56:51 +0900

    CREATE; feed model

---
+++ src/main/java/com/semicolon/spring/entity/feed/Feed.java
@@ -0,0 +1,36 @@
+package com.semicolon.spring.entity.feed;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.semicolon.spring.entity.club.Club;
+import com.semicolon.spring.entity.feed_medium.FeedMedium;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "feed")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Feed {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String contents;
+
+    private boolean pin;
+
+    @OneToMany(mappedBy = "feed_id", fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<FeedMedium> media;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    @JsonBackReference
+    private Club club_id;
+}
