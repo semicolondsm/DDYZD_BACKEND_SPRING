b43ea230247e72b98f5ca3dccf2dabb8c7577641
leeseojune <sung07288346@gmail.com>
2021-01-17 22:39:24 +0900

    CREATE; FeedController

---
+++ src/main/java/com/semicolon/spring/controller/FeedController.java
@@ -0,0 +1,9 @@
+package com.semicolon.spring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FeedController {
+}
