92eca81f0aa29f32eef10918446bb0db51cd2c3f
leeseojune <sung07288346@gmail.com>
2021-01-17 20:57:45 +0900

    CREATE; feed_medium

---
+++ src/main/java/com/semicolon/spring/entity/feed_medium/FeedMedium.java
@@ -0,0 +1,29 @@
+package com.semicolon.spring.entity.feed_medium;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.semicolon.spring.entity.feed.Feed;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "feed_medium")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class FeedMedium {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String medium_path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    @JsonBackReference
+    private Feed feed_id;
+
+}
