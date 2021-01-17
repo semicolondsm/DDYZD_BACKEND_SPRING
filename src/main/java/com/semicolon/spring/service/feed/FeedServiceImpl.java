67e31ef2217de26a8144c7fbddf3d232c1de638d
leeseojune <sung07288346@gmail.com>
2021-01-17 22:39:34 +0900

    CREATE; FeedServiceImpl

---
+++ src/main/java/com/semicolon/spring/service/feed/FeedServiceImpl.java
@@ -0,0 +1,10 @@
+package com.semicolon.spring.service.feed;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FeedServiceImpl implements FeedService{
+    @Override
+    public void fileUpload(MultipartFile file) {
+
+    }
+}
