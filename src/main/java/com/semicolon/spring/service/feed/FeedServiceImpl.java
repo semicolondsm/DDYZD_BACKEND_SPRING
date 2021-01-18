cb3f743d07729b003aa5cfd5773ea7a03bcd1d41
leeseojune <sung07288346@gmail.com>
2021-01-18 22:51:05 +0900

    UPDATE; FeedServiceImpl

--- src/main/java/com/semicolon/spring/service/feed/FeedServiceImpl.java
+++ src/main/java/com/semicolon/spring/service/feed/FeedServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -43,9 +45,41 @@ public class FeedServiceImpl implements FeedService{
                     Feed.builder()
                         .contents(request.getContent())
                         .pin(request.isPin())
-                        .club_id(clubRepository.findById(1).orElseThrow(RuntimeException::new))
+                        .club_id(clubRepository.findById(1).orElseThrow(RuntimeException::new)) // 차후에 수정 필요
                         .build()
                 ).getId());
 
         }
+
+    @Override
+    public List<FeedDTO.getFeed> getFeedList(int page) {
+        return feedToRepose(feedRepository.findByIdLessThanOrderByIdDesc(page));
     }
+
+    public List<FeedDTO.getFeed> feedToRepose(List<Feed> feeds){
+        List<FeedDTO.getFeed> response = new ArrayList<>();
+        for(int i=0;i<feeds.size();i++){
+            Feed feed = feeds.get(i);
+            response.add(
+                    FeedDTO.getFeed.builder()
+                    .feedId(feed.getId())
+                    .clubName(feed.getClub_id().getClub_name())
+                    .profileImage(feed.getClub_id().getProfile_image())
+                    .content(feed.getContents())
+                    .media(getMediaPath(feed.getMedia()))
+                    .uploadAt(feed.getUploadAt())
+                    .build()
+            );
+        }
+        return response;
+    }
+
+    public List<String> getMediaPath(List<FeedMedium> feedMedia){
+        List<String> response = new ArrayList<>();
+        for(int i=0;i<feedMedia.size();i++){
+            FeedMedium medium = feedMedia.get(i);
+            response.add(medium.getMedium_path());
+        }
+        return response;
+    }
+}
