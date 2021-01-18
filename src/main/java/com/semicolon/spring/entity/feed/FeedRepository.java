c16374eba7e344a8704b0b05818a80986e4fd119
leeseojune <sung07288346@gmail.com>
2021-01-18 22:50:43 +0900

    UPDATE; FeedRepository

--- src/main/java/com/semicolon/spring/entity/feed/FeedRepository.java
+++ src/main/java/com/semicolon/spring/entity/feed/FeedRepository.java
@@ -3,6 +3,10 @@ package com.semicolon.spring.entity.feed;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FeedRepository extends CrudRepository<Feed, Integer> {
+    List<Feed> findByIdLessThanOrderByIdDesc(int Id);
 }
