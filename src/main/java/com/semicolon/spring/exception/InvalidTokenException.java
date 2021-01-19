6cc17dd29dedcb761340a3589fa2010c0f65bc0b
leeseojune <sung07288346@gmail.com>
2021-01-20 02:33:28 +0900

    UPDATE; InvalidTokenException

--- src/main/java/com/semicolon/spring/exception/InvalidTokenException.java
+++ src/main/java/com/semicolon/spring/exception/InvalidTokenException.java
@@ -3,7 +3,7 @@ package com.semicolon.spring.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "InvalidToken")
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "InvalidToken")
 public class InvalidTokenException extends RuntimeException{
     public InvalidTokenException(){
         super("InvalidTokenException");
