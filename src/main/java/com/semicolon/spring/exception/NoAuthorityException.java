dab36c1ab2e6e885a0b5f3b3c5d41ad075d8a538
leeseojune <sung07288346@gmail.com>
2021-01-20 02:33:40 +0900

    UPDATE; NoAuthorityException

--- src/main/java/com/semicolon/spring/exception/NoAuthorityException.java
+++ src/main/java/com/semicolon/spring/exception/NoAuthorityException.java
@@ -3,7 +3,7 @@ package com.semicolon.spring.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "NoAuthority")
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "NoAuthority")
 public class NoAuthorityException extends RuntimeException{
     public NoAuthorityException(){
         super("NoAuthorityException");
