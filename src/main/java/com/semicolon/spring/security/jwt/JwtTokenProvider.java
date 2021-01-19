7a2ed5ddd071fce9b4ea9e92681865737452794c
leeseojune <sung07288346@gmail.com>
2021-01-20 00:58:34 +0900

    CREATE; JwtTokenProvider

---
+++ src/main/java/com/semicolon/spring/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,67 @@
+package com.semicolon.spring.security.jwt;
+
+import com.semicolon.spring.exception.InvalidTokenException;
+import com.semicolon.spring.security.jwt.auth.AuthDetails;
+import com.semicolon.spring.security.jwt.auth.AuthDetailsService;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.prefix}")
+    private String prefix;
+
+    @Value("${jwt.header}")
+    private String header;
+
+    @Value("${jwt.exp.access}")
+    private Long accessTokenExpiration;
+
+    @Value("${jwt.exp.refresh}")
+    private Long refreshTokenExpiration;
+
+    private final AuthDetailsService authDetailsService;
+
+    public String resolveToken(HttpServletRequest request){
+        String bearer = request.getHeader(header);
+        if(bearer!=null && bearer.startsWith(prefix)){
+            return bearer.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser()
+                    .setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+            return true;
+        }catch (Exception e){
+            throw new InvalidTokenException();
+        }
+    }
+
+    public String getId(String token){
+        try{
+            return Jwts.parser()
+                    .setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        }catch (Exception e){
+            throw new InvalidTokenException();
+        }
+    }
+
+    public Authentication authentication(String token){
+        AuthDetails authDetails = authDetailsService.loadUserByUsername(getId(token));
+        return new UsernamePasswordAuthenticationToken(authDetails, "", authDetails.getAuthorities());
+    }
+}
