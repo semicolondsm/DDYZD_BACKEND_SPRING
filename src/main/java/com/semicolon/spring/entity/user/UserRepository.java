44d59b1b0634833f4990c12c28958ea6156cb273
leeseojune <sung07288346@gmail.com>
2021-01-21 02:42:43 +0900

    UPDATE; UserRepository

--- src/main/java/com/semicolon/spring/entity/user/UserRepository.java
+++ src/main/java/com/semicolon/spring/entity/user/UserRepository.java
@@ -3,7 +3,6 @@ package com.semicolon.spring.entity.user;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
