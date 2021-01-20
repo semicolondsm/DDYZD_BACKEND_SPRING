6ff23ea5c296d2e8692babcb5e0bba369965c492
leeseojune <sung07288346@gmail.com>
2021-01-20 19:56:23 +0900

    UPDATE; ClubController

--- src/main/java/com/semicolon/spring/controller/ClubController.java
+++ src/main/java/com/semicolon/spring/controller/ClubController.java
@@ -1,9 +1,23 @@
 package com.semicolon.spring.controller;
 
+import com.semicolon.spring.dto.ClubDTO;
+import com.semicolon.spring.service.club_head.ClubHeadService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 public class ClubController {
+    private final ClubHeadService clubHeadService;
+
+    @PostMapping("/club/{club_id}/recruitment")
+    public ClubDTO.messageResponse recruitment(@RequestBody ClubDTO.recruitment request, @PathVariable("club_id") int club_id){
+        return clubHeadService.recruitment(request, club_id);
+    }
+
+    @PostMapping("/club/{club_id}/profile")
+    public ClubDTO.messageResponse profile(@RequestParam("file")MultipartFile file, @PathVariable("club_id") int club_id){
+        return clubHeadService.clubProfile(file, club_id);
+    }
 }
