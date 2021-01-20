bf021f8c40d7bc4da90be206593a6d63cfd9eda6
leeseojune <sung07288346@gmail.com>
2021-01-20 20:27:18 +0900

    UPDATE; ClubHeadService

--- src/main/java/com/semicolon/spring/service/club_head/ClubHeadServiceImpl.java
+++ src/main/java/com/semicolon/spring/service/club_head/ClubHeadServiceImpl.java
@@ -66,6 +66,25 @@ public class ClubHeadServiceImpl implements ClubHeadService{
         }
     }
 
+    @Override
+    public ClubDTO.messageResponse clubHongbo(MultipartFile file, int club_id) {
+        if(!isClubHead(club_id))
+            throw new NoAuthorityException();
+        try{
+            file.transferTo(new File(PATH+file.getOriginalFilename()));
+            clubRepository.findById(club_id)
+                    .map(club -> {
+                        club.setHongbo_image(PATH+file.getOriginalFilename());
+                        clubRepository.save(club);
+                        return club;
+                    });
+            return new ClubDTO.messageResponse("club hongbo write success");
+        }catch (IOException e){
+            e.printStackTrace();
+            throw new FileNotSaveException();
+        }
+    }
+
     private boolean isClubHead(int club_id){
         User user = authenticationFacade.getUser();
         return clubRepository.findById(club_id)
