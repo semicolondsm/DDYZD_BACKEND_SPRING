70dff59eef7a67963528b8cd1ab4b0253ca3289c
leeseojune <sung07288346@gmail.com>
2021-01-21 02:42:16 +0900

    CREATE; ClubFollow

---
+++ src/main/java/com/semicolon/spring/entity/club/club_follow/ClubFollow.java
@@ -0,0 +1,29 @@
+package com.semicolon.spring.entity.club.club_follow;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.semicolon.spring.entity.club.Club;
+import com.semicolon.spring.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "club_follow")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClubFollow {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer follow_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    @JsonBackReference
+    private Club club;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+}
