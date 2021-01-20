aa1ebb8f6da2d85cc8249d19bbd9ca96c30a474f
leeseojune <sung07288346@gmail.com>
2021-01-20 21:55:36 +0900

    UPDATE; FeedServiceImpl

--- src/main/java/com/semicolon/spring/service/feed/FeedServiceImpl.java
+++ src/main/java/com/semicolon/spring/service/feed/FeedServiceImpl.java
@@ -35,6 +35,8 @@ public class FeedServiceImpl implements FeedService{
     @Value("${file.path}")
     private String PATH;
 
+    //Security Context에서 가져오는 User정보가 null이 아니라면 is follow와 isflag를 return한다. 만약 User정보가 null이라면 둘 다 false를 return한다.
+
     @Override
     public void fileUpload(MultipartFile file, int feedId) { // feed가 자기 클럽이 쓴것인지 확인.
         try{
@@ -51,7 +53,7 @@ public class FeedServiceImpl implements FeedService{
     }
 
     @Override
-    public FeedDTO.writeFeedResponse writeFeed(FeedDTO.feed request) {
+    public FeedDTO.writeFeedResponse writeFeed(FeedDTO.feed request, int club_id) {
         return new FeedDTO.writeFeedResponse("feed writing success",
                 feedRepository.save(
                     Feed.builder()
