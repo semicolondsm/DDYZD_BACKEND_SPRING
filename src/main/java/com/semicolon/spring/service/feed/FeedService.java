f6223684a2d2dead56b98bd79197a2544e533c79
leeseojune <sung07288346@gmail.com>
2021-01-20 21:55:29 +0900

    UPDATE; FeedService

--- src/main/java/com/semicolon/spring/service/feed/FeedService.java
+++ src/main/java/com/semicolon/spring/service/feed/FeedService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface FeedService {
     void fileUpload(MultipartFile file, int feedId);
-    FeedDTO.writeFeedResponse writeFeed(FeedDTO.feed request);
+    FeedDTO.writeFeedResponse writeFeed(FeedDTO.feed request, int club_id);
     List<FeedDTO.getFeed> getFeedList(int page);
     List<FeedDTO.getFeedClub> getFeedClubList(int page, int club_id);
     FeedDTO.messageResponse feedModify(FeedDTO.feed request, int feedId);
