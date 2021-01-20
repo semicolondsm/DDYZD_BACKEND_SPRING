5f1c28a1e6c046b1cf6084baa56391758f9ff801
leeseojune <sung07288346@gmail.com>
2021-01-20 19:20:04 +0900

    UPDATE; ClubHeadService

--- src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
+++ src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
@@ -1,7 +1,10 @@
 package com.semicolon.spring.service.club_head;
 
 import com.semicolon.spring.dto.ClubDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ClubHeadService {
     ClubDTO.messageResponse recruitment(ClubDTO.recruitment request, int club_id);
+    ClubDTO.messageResponse clubProfile(MultipartFile file, int club_id);
+
 }
