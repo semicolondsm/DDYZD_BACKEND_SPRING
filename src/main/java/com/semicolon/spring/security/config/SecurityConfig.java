92a4e3ab5736d1d8b51e44034d4f14f9560fd9a3
leeseojune <sung07288346@gmail.com>
2021-01-22 03:13:04 +0900

    UPDATE; SecurityConfig

--- src/main/java/com/semicolon/spring/security/config/SecurityConfig.java
+++ src/main/java/com/semicolon/spring/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.semicolon.spring.security.config;
 
+import com.semicolon.spring.security.jwt.JwtConfigure;
+import com.semicolon.spring.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +13,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .formLogin().disable()
                 .cors().disable()
                 .csrf().disable();
-        http
-                .authorizeRequests().antMatchers("/**").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/feed/list").permitAll()
+                .antMatchers("/feed/{club_id}/list").permitAll()
+                .anyRequest().authenticated()
+                .and().apply(new JwtConfigure(jwtTokenProvider));
+
     }
 }
