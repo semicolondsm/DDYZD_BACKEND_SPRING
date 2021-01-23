146aba7200fbde44680a1e957ee741f45fbb201b
leeseojune <sung07288346@gmail.com>
2021-01-24 03:02:52 +0900

    UPDATE; JwtConfigure

--- src/main/java/com/semicolon/spring/security/jwt/JwtConfigure.java
+++ src/main/java/com/semicolon/spring/security/jwt/JwtConfigure.java
@@ -5,9 +5,11 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
-public class JwtConfigure extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+public class JwtConfigure extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> implements WebMvcConfigurer {
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -15,5 +17,10 @@ public class JwtConfigure extends SecurityConfigurerAdapter<DefaultSecurityFilte
     public void configure(HttpSecurity builder) throws Exception {
         JwtTokenFilter tokenFilter = new JwtTokenFilter(jwtTokenProvider);
         builder.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
