6db63c58f286ea7799496ec45cc454df0af03904
leeseojune <sung07288346@gmail.com>
2021-01-24 03:07:38 +0900

    CREATE; ErrorResponse

---
+++ src/main/java/com/semicolon/spring/error/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.semicolon.spring.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+}
