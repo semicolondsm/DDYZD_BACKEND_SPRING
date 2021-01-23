92b443949ebbc12dd395edeeb2aa4ea9d88608cc
leeseojune <sung07288346@gmail.com>
2021-01-24 03:07:48 +0900

    CREATE; BusinessException

---
+++ src/main/java/com/semicolon/spring/error/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.semicolon.spring.error.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException{
+
+    private ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode){
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
