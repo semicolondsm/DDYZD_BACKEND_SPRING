daa2ae465a6f6844dc5e5f9394add845c439ad63
leeseojune <sung07288346@gmail.com>
2021-01-25 21:05:09 +0900

    UPDATE; cors config

--- src/main/java/com/semicolon/spring/security/config/SecurityConfig.java
+++ src/main/java/com/semicolon/spring/security/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -29,5 +30,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .anyRequest().authenticated()
                 .and().apply(new JwtConfigure(jwtTokenProvider));
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
