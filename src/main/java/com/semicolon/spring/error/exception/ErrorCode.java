45e4c0c576803548e3015580c199947048bfbd1c
leeseojune <sung07288346@gmail.com>
2021-02-17 21:20:13 +0900

    UPDATE; ErrorCode

--- src/main/java/com/semicolon/spring/error/exception/ErrorCode.java
+++ src/main/java/com/semicolon/spring/error/exception/ErrorCode.java
@@ -24,7 +24,9 @@ public enum ErrorCode {
     FEED_NOT_FOUND(404, "Feed Not Found."),
 
     //File
-    FILE_SAVE_FAIL(400, "File Save Fail.");
+    FILE_SAVE_FAIL(400, "File Save Fail."),
+    FILE_NOT_FOUND(404, "File Not Fail."),
+    BAD_FILE_EXTENSION(400, "Bad File Extension");
 
     private final int status;
     private final String message;
