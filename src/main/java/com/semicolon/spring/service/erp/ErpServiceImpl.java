909f46990da55ee5d71c5b27678cab8d96c0587e
leeseojune <sung07288346@gmail.com>
2021-02-23 18:27:44 +0900

    UPDATE; ErpServiceImpl

--- src/main/java/com/semicolon/spring/service/erp/ErpServiceImpl.java
+++ src/main/java/com/semicolon/spring/service/erp/ErpServiceImpl.java
@@ -16,9 +16,16 @@ import java.util.List;
 public class ErpServiceImpl implements ErpService{
     @Override
     public ErpDTO.Supply supply(ErpDTO.Url inputUrl) throws IOException {
-        if(inputUrl.getUrl().startsWith("http://emart.ssg.com/")){
+        if(inputUrl.getUrl().contains("emart.ssg.com")){
             return emart(inputUrl.getUrl());
-        }else throw new BadSupplyLinkException();
+        }else if(inputUrl.getUrl().contains("www.yes24.com")) {
+            return yes24(inputUrl.getUrl());
+        }else if(inputUrl.getUrl().contains("www.popcone.co.kr")){
+            return popcone(inputUrl.getUrl());
+        }else if(inputUrl.getUrl().contains("www.s2b.kr")){
+            return s2b(inputUrl.getUrl());
+        }
+        else throw new BadSupplyLinkException();
     }
 
     private ErpDTO.Supply emart(String url) throws IOException{
@@ -40,6 +47,53 @@ public class ErpServiceImpl implements ErpService{
 
 
         return new ErpDTO.Supply(name.text(), options, images);
+    }
+
+    private ErpDTO.Supply yes24(String url) throws IOException{
+        Document doc = Jsoup.connect(url).get();
+
+        Element name = doc.selectFirst("title");
+
+        Elements image = doc.select("em.imgBdr");
+
+        List<String> images = new ArrayList<>();
+
+        for(Element e :image.select("img")){
+            images.add(e.text());
+        }
+
+        return new ErpDTO.Supply(name.text(), new ArrayList<>(), images);
+    }
+
+    private ErpDTO.Supply popcone(String url) throws IOException{
+        Document doc = Jsoup.connect(url).get();
+
+        Element name = doc.selectFirst("div.desc_ti h2");
+
+        Element image = doc.selectFirst("div.obj_pic");
+
+        List<String> images = new ArrayList<>();
+        for(Element e : image.select("img")){
+            images.add(e.absUrl("src"));
+        }
+
+        return new ErpDTO.Supply(name.text(), new ArrayList<>(), images);
+
+    }
+
+    private ErpDTO.Supply s2b(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+
+        Element name = doc.selectFirst("font.f12_b_black");
+
+        Element image = doc.selectFirst("td.detail_img");
+
+        List<String> images = new ArrayList<>();
+        for(Element e : image.select("img")){
+            images.add(e.absUrl("src"));
+        }
+
+        return new ErpDTO.Supply(name.text(), new ArrayList<>(), images);
     }
 
 }
