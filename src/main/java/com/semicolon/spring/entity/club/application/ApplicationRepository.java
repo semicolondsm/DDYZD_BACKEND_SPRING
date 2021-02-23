c855b22f0f957ee79e2d98726463a7422f31c988
leeseojune <sung07288346@gmail.com>
2021-02-23 18:26:49 +0900

    UPDATE; ApplicationRepository

--- src/main/java/com/semicolon/spring/entity/club/application/ApplicationRepository.java
+++ src/main/java/com/semicolon/spring/entity/club/application/ApplicationRepository.java
@@ -5,8 +5,12 @@ import com.semicolon.spring.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 
 @Repository
 public interface ApplicationRepository extends CrudRepository<Application, Integer> {
     Application findByUserAndClub(User user, Club club);
+    @Transactional
+    void deleteByUserAndClub(User user, Club club);
 }
