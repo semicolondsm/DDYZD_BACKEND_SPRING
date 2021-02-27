30726ec3df1cbf594be72516229020b897da0d15
leeseojune <sung07288346@gmail.com>
2021-02-28 04:45:29 +0900

    UPDATE; HeadDTO

--- src/main/java/com/semicolon/spring/dto/HeadDTO.java
+++ src/main/java/com/semicolon/spring/dto/HeadDTO.java
@@ -1,15 +1,26 @@
 package com.semicolon.spring.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-public class ApplicationDTO {
+public class HeadDTO {
 
     @AllArgsConstructor
     @NoArgsConstructor
     @Getter
     public static class MessageResponse{
+        private String message;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Builder
+    public static class FcmRequest{
+        private String token;
+        private String title;
         private String message;
     }
 
