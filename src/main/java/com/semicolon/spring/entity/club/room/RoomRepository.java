4be323654ce8992dc170de2f9e25ac971025ecc9
leeseojune <sung07288346@gmail.com>
2021-02-28 00:17:29 +0900

    CREATE; RoomRepository

---
+++ src/main/java/com/semicolon/spring/entity/club/room/RoomRepository.java
@@ -0,0 +1,6 @@
+package com.semicolon.spring.entity.club.room;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Integer> {
+}
