5b0abf5ae41c563cce1174104f6df922e26ce8ff
leeseojune <sung07288346@gmail.com>
2021-03-01 14:55:46 +0900

    UPDATE; ApplicationController test

--- src/main/java/com/semicolon/spring/controller/ApplicationController.java
+++ src/main/java/com/semicolon/spring/controller/ApplicationController.java
@@ -2,11 +2,9 @@ package com.semicolon.spring.controller;
 
 import com.semicolon.spring.dto.HeadDTO;
 import com.semicolon.spring.service.application.ApplicationService;
+import com.semicolon.spring.service.fcm.FcmService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
 
@@ -16,6 +14,7 @@ import java.util.concurrent.ExecutionException;
 public class ApplicationController {
 
     private final ApplicationService applicationService;
+    private final FcmService fcmService;
 
     @DeleteMapping("/cancel")
     public HeadDTO.MessageResponse cancelApplication(@RequestParam("clubid") int club_id) throws ExecutionException, InterruptedException {
@@ -30,6 +29,16 @@ public class ApplicationController {
     @DeleteMapping("/deport")
     public HeadDTO.MessageResponse deportApplication(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id){
         return applicationService.deportApplication(club_id, user_id);
+    }
+
+    @GetMapping("/test")
+    public void test() throws ExecutionException, InterruptedException {
+        fcmService.send(HeadDTO.FcmRequest.builder()
+                .token("c9gCLOJY1EATrHG1a_Gfux:APA91bFdHLmqwqfniOtctK6V4tEpaKkl2UkNTf-TM_FVgges6AYkSFXBiTviyoRY1UnNrpuRseJx2tfXmJl-MZW2HSHneujd8kFGsIji63ZcWJHefjX9JDtDsmjPNmmuFcRqchRNtmLx")
+                .title("semicolon;")
+                .message("테스트임니다 ㅎㅎ")
+                .build()
+        );
     }
 
 }
