f83d2f6134d14fe1d6b05c97cc97c4f912e97947
leeseojune <sung07288346@gmail.com>
2021-03-02 22:19:52 +0900

    UPDATE; ClubHeadService

--- src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
+++ src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
@@ -3,8 +3,10 @@ package com.semicolon.spring.service.club_head;
 import com.semicolon.spring.dto.ClubDTO;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.concurrent.ExecutionException;
+
 public interface ClubHeadService {
-    ClubDTO.messageResponse recruitment(ClubDTO.recruitment request, int club_id);
+    ClubDTO.messageResponse recruitment(ClubDTO.recruitment request, int club_id) throws ExecutionException, InterruptedException;
     ClubDTO.messageResponse clubProfile(MultipartFile file, int club_id);
     ClubDTO.messageResponse clubHongbo(MultipartFile file, int club_id);
     ClubDTO.messageResponse clubBanner(MultipartFile file, int club_id);
