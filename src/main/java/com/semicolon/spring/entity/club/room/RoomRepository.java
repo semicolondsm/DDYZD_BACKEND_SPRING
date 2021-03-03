70dffa5caac6a15109f5c3d867dce322c248180b
leeseojune <sung07288346@gmail.com>
2021-03-03 21:28:14 +0900

    UPDATE; RoomRepository

--- src/main/java/com/semicolon/spring/entity/club/room/RoomRepository.java
+++ src/main/java/com/semicolon/spring/entity/club/room/RoomRepository.java
@@ -5,9 +5,12 @@ import com.semicolon.spring.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface RoomRepository extends CrudRepository<Room, Integer> {
     Room findByUserAndClub(User user, Club club);
+
+    List<Room> findByClub(Club club);
 
     @Transactional
     void deleteByUserAndClub(User user, Club club);
