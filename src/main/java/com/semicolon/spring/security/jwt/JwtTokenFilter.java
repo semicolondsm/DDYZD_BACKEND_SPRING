5c38568b5892285d093fe6e2a425dc3bf790e61c
leeseojune <sung07288346@gmail.com>
2021-03-06 20:40:32 +0900

    UPDATE; JwtTokenFilter

--- src/main/java/com/semicolon/spring/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/semicolon/spring/security/jwt/JwtTokenFilter.java
@@ -2,6 +2,7 @@ package com.semicolon.spring.security.jwt;
 
 import com.semicolon.spring.exception.InvalidTokenException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenFilter extends GenericFilterBean {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -23,6 +25,7 @@ public class JwtTokenFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
             String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+            log.info(token);
             if (token != null && jwtTokenProvider.validateToken(token)) {
                 Authentication auth = jwtTokenProvider.authentication(token);
                 SecurityContextHolder.getContext().setAuthentication(auth);
