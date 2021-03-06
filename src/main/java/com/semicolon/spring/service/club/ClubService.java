a40d25bed908bc5f4d51eefb809cf329364fe128
leeseojune <sung07288346@gmail.com>
2021-03-06 15:21:38 +0900

    UPDATE; ClubService

--- src/main/java/com/semicolon/spring/service/club/ClubService.java
+++ src/main/java/com/semicolon/spring/service/club/ClubService.java
@@ -2,6 +2,9 @@ package com.semicolon.spring.service.club;
 
 import com.semicolon.spring.dto.ClubDTO;
 
+import java.util.List;
+
 public interface ClubService {
     ClubDTO.followers getFollowers(int club_id);
+    List<ClubDTO.club> getClubs();
 }
