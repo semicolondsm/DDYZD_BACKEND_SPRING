31a79251a2e1fa479caea202eea14f488b5763b8
leeseojune <sung07288346@gmail.com>
2021-03-08 17:03:32 +0900

    UPDATE; ErrorCode

--- src/main/java/com/semicolon/spring/error/exception/ErrorCode.java
+++ src/main/java/com/semicolon/spring/error/exception/ErrorCode.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 public enum ErrorCode {
     //Common
     BAD_REQUEST(400, "BadRequest."),
+    NOT_FOUND(404, "Not Found"),
     USER_NOT_FOUND(404, "User Not Found."),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error."),
 
