6d250bd438225a3d77559bdb7f989c6a65c16e6f
leeseojune <sung07288346@gmail.com>
2021-03-08 17:03:46 +0900

    FIX; FcmService

--- src/main/java/com/semicolon/spring/service/fcm/FcmService.java
+++ src/main/java/com/semicolon/spring/service/fcm/FcmService.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.*;
 import com.semicolon.spring.dto.HeadDTO;
+import com.semicolon.spring.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -33,26 +34,31 @@ public class FcmService {
         }
     }
 
-    public void send(HeadDTO.FcmRequest request) throws ExecutionException, InterruptedException {
-        Message message = Message.builder()
-                .setToken(request.getToken())
-                .putData("club_id", request.getClub().toString())
-                .setNotification(Notification.builder() // setImage추가하기.
-                        .setTitle(request.getTitle())
-                        .setBody(request.getMessage())
-                        .build()
-                )
-                .setApnsConfig(ApnsConfig.builder()
-                        .setAps(Aps.builder()
-                                .setSound("default")
-                                .build()
-                        ).build()
-                )
-                .build();
+    public void send(HeadDTO.FcmRequest request) {
+        try{
+            Message message = Message.builder()
+                    .setToken(request.getToken())
+                    .putData("club_id", request.getClub().toString())
+                    .setNotification(Notification.builder() // setImage추가하기.
+                            .setTitle(request.getTitle())
+                            .setBody(request.getMessage())
+                            .build()
+                    )
+                    .setApnsConfig(ApnsConfig.builder()
+                            .setAps(Aps.builder()
+                                    .setSound("default")
+                                    .build()
+                            ).build()
+                    )
+                    .build();
 
 
-        String response = FirebaseMessaging.getInstance().sendAsync(message).get();
-        log.info("Sent Message" + response);
+            String response = FirebaseMessaging.getInstance().sendAsync(message).get();
+            log.info("Sent Message" + response);
+        }catch (Exception e){
+            throw new NotFoundException();
+        }
+
     }
 
 }
