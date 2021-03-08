823e9c19f4a68489971fe77891505308f1f08963
leeseojune <sung07288346@gmail.com>
2021-03-08 23:46:29 +0900

    UPDATE; FeedRepository

--- src/main/java/com/semicolon/spring/entity/feed/FeedRepository.java
+++ src/main/java/com/semicolon/spring/entity/feed/FeedRepository.java
@@ -3,15 +3,20 @@ package com.semicolon.spring.entity.feed;
 import com.semicolon.spring.entity.club.Club;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 
 @Repository
-public interface FeedRepository extends CrudRepository<Feed, Integer> {
-    Page<Feed> findAll(Pageable pageable);
+public interface FeedRepository extends JpaRepository<Feed, Integer> {
+
+    Page<Feed> findAllByOrderByIdDesc(Pageable pageable);
+
     Page<Feed> findByClub(Club club, Pageable pageable);
     List<Feed> findByClubAndPinIsTrue(Club club);
 }
