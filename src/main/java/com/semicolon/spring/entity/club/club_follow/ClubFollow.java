7a970e92c7ba88cd17d7ba39a8fcd26e803d7830
leeseojune <sung07288346@gmail.com>
2021-03-09 14:27:03 +0900

    UPDATE; ClubFollow model

--- src/main/java/com/semicolon/spring/entity/club/club_follow/ClubFollow.java
+++ src/main/java/com/semicolon/spring/entity/club/club_follow/ClubFollow.java
@@ -11,6 +11,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @Entity(name = "club_follow")
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"club_id", "user_id"}))
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
