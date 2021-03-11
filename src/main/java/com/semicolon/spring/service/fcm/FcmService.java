fbfa41aeff47a4b04fa422e22f5d31644d452f6d
leeseojune <sung07288346@gmail.com>
2021-03-11 19:01:53 +0900

    UPDATE; FcmService

--- src/main/java/com/semicolon/spring/service/fcm/FcmService.java
+++ src/main/java/com/semicolon/spring/service/fcm/FcmService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 @Service
 @Slf4j
@@ -57,7 +56,7 @@ public class FcmService {
                 String response = FirebaseMessaging.getInstance().sendAsync(message).get();
                 log.info("Sent Message" + response);
             }
-            
+
         }catch (Exception e){
             log.error(e.getMessage());
             throw new NotFoundException();
