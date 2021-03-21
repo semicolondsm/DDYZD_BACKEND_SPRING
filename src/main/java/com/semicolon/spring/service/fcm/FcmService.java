d882321de7ee60b5ff8b9b287d769c14d422c44d
leeseojune <sung07288346@gmail.com>
2021-03-22 08:41:38 +0900

    UPDATE; FcmService

--- src/main/java/com/semicolon/spring/service/fcm/FcmService.java
+++ src/main/java/com/semicolon/spring/service/fcm/FcmService.java
@@ -71,7 +71,7 @@ public class FcmService {
 
     }
 
-    @Scheduled(cron = "0 40 8 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 43 8 * * *", zone = "Asia/Seoul")
     public void sendSelfDiagnosis(){
 //        for(User user : userRepository.findAll()){
 //            if(user.getDevice_token() != null){
