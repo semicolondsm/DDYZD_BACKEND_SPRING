741205a8414c6037679e991ecf7686be0f1a4ad5
leeseojune <sung07288346@gmail.com>
2021-03-22 08:40:45 +0900

    UPDATE; FcmService

--- src/main/java/com/semicolon/spring/service/fcm/FcmService.java
+++ src/main/java/com/semicolon/spring/service/fcm/FcmService.java
@@ -5,18 +5,25 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.*;
 import com.semicolon.spring.dto.HeadDTO;
+import com.semicolon.spring.entity.user.User;
+import com.semicolon.spring.entity.user.UserRepository;
 import com.semicolon.spring.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.Date;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class FcmService {
 
+    private final UserRepository userRepository;
     private static final String FIREBASE_CONFIG_PATH = "ddyzd-firebase-adminsdk.json";
 
     @PostConstruct
@@ -62,6 +69,22 @@ public class FcmService {
             throw new NotFoundException();
         }
 
+    }
+
+    @Scheduled(cron = "0 40 8 * * *", zone = "Asia/Seoul")
+    public void sendSelfDiagnosis(){
+//        for(User user : userRepository.findAll()){
+//            if(user.getDevice_token() != null){
+//                this.send(HeadDTO.FcmRequest.builder()
+//                        .title(user.getName() + "님 자가진단 해주세요.")
+//                        .club(19)
+//                        .token(user.getDevice_token())
+//                        .message("자가진단 실시해주세요.")
+//                        .build()
+//                );
+//            }
+//        }
+        log.info(String.valueOf(new Date()));
     }
 
 }
