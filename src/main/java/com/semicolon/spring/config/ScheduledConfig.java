94c3070988f71e8ea60528a5b8f588842b92fec3
leeseojune <sung07288346@gmail.com>
2021-03-22 08:40:22 +0900

    CREATE; ScheduledConfig

---
+++ src/main/java/com/semicolon/spring/config/ScheduledConfig.java
@@ -0,0 +1,19 @@
+package com.semicolon.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class ScheduledConfig {
+
+    @Bean
+    public TaskScheduler scheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(4);
+        return scheduler;
+    }
+}
