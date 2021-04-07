17f4e35eef3491454437879fa4a7cb6cd94aac94
leeseojune <sung07288346@gmail.com>
2021-04-07 17:22:59 +0900

    UPDATE; ClubHeadService

--- src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
+++ src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
@@ -3,9 +3,9 @@ package com.semicolon.spring.service.club_head;
 import com.semicolon.spring.dto.ClubDTO;
 
 public interface ClubHeadService {
-    ClubDTO.messageResponse deportMember(int club_id, int user_id);
-    ClubDTO.messageResponse insertMember(int club_id, int user_id);
-    ClubDTO.messageResponse changeHead(ClubDTO.changeHead request, int club_id);
-    ClubDTO.messageResponse insertManager(int club_id, int user_id);
-    ClubDTO.messageResponse deportManager(int club_id, int user_id);
+    ClubDTO.MessageResponse deportMember(int club_id, int user_id);
+    ClubDTO.MessageResponse insertMember(int club_id, int user_id);
+    ClubDTO.MessageResponse changeHead(ClubDTO.ChangeHead request, int club_id);
+    ClubDTO.MessageResponse insertManager(int club_id, int user_id);
+    ClubDTO.MessageResponse deportManager(int club_id, int user_id);
 }
