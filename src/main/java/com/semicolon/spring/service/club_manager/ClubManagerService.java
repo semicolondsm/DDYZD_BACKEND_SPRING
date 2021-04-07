f273e7f1828ce0fba528521c4db452ae2b598038
leeseojune <sung07288346@gmail.com>
2021-04-07 17:23:22 +0900

    UPDATE; ClubManagerService

--- src/main/java/com/semicolon/spring/service/club_manager/ClubManagerService.java
+++ src/main/java/com/semicolon/spring/service/club_manager/ClubManagerService.java
@@ -6,15 +6,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.concurrent.ExecutionException;
 
 public interface ClubManagerService {
-    ClubDTO.messageResponse recruitment(ClubDTO.recruitment request, int club_id) throws ExecutionException, InterruptedException;
-    ClubDTO.messageResponse clubProfile(MultipartFile file, int club_id);
-    ClubDTO.messageResponse clubHongbo(MultipartFile file, int club_id);
-    ClubDTO.messageResponse clubBanner(MultipartFile file, int club_id);
-    ClubDTO.messageResponse modifyClub(ClubDTO.modify request, int club_id);
-    ClubDTO.messageResponse clubDescription(ClubDTO.description request, int club_id);
-    ClubDTO.messageResponse deleteRecruitment(int club_id);
-    ClubDTO.hongbo getHongbo(int club_id);
-    ClubDTO.messageResponse deleteHongbo(int club_id);
+    ClubDTO.MessageResponse recruitment(ClubDTO.Recruitment request, int club_id) throws ExecutionException, InterruptedException;
+    ClubDTO.MessageResponse clubProfile(MultipartFile file, int club_id);
+    ClubDTO.MessageResponse clubHongbo(MultipartFile file, int club_id);
+    ClubDTO.MessageResponse clubBanner(MultipartFile file, int club_id);
+    ClubDTO.MessageResponse modifyClub(ClubDTO.Modify request, int club_id);
+    ClubDTO.MessageResponse clubDescription(ClubDTO.Description request, int club_id);
+    ClubDTO.MessageResponse deleteRecruitment(int club_id);
+    ClubDTO.Hongbo getHongbo(int club_id);
+    ClubDTO.MessageResponse deleteHongbo(int club_id);
 
 
 }
