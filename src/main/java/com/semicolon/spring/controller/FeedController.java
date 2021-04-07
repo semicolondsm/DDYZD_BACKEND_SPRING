2ff2dac07fb00d05ddf1f8559d5ac88f3c4d37fa
leeseojune <sung07288346@gmail.com>
2021-04-07 17:22:20 +0900

    UPDATE; FeedController

--- src/main/java/com/semicolon/spring/controller/FeedController.java
+++ src/main/java/com/semicolon/spring/controller/FeedController.java
@@ -1,9 +1,8 @@
 package com.semicolon.spring.controller;
 
-import com.semicolon.spring.dto.FeedDTO;
+import com.semicolon.spring.dto.FeedDTO.*;
 import com.semicolon.spring.exception.BadRequestException;
 import com.semicolon.spring.service.feed.FeedService;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,18 +20,18 @@ public class FeedController {
 
     @PostMapping("/feed/{feedId}/medium")
     @ResponseStatus(HttpStatus.CREATED)
-    public FeedDTO.messageResponse fileUpload(@RequestParam("files") List<MultipartFile> files, @PathVariable("feedId") int feedId){
+    public MessageResponse fileUpload(@RequestParam("files") List<MultipartFile> files, @PathVariable("feedId") int feedId){
         return feedService.fileUpload(files, feedId);
     }
 
     @PostMapping("/feed/{club_id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public FeedDTO.writeFeedResponse feedUpload(@RequestBody FeedDTO.feed request, @PathVariable("club_id") int club_id){
+    public WriteFeedResponse feedUpload(@RequestBody Feed request, @PathVariable("club_id") int club_id){
         return feedService.writeFeed(request, club_id);
     }
 
     @GetMapping("/feed/list")
-    public List<FeedDTO.getFeed> getFeedList(@RequestParam("page") int page){
+    public List<GetFeed> getFeedList(@RequestParam("page") int page){
         if(page < 0){
             throw new BadRequestException();
         }
@@ -41,7 +39,7 @@ public class FeedController {
     }
 
     @GetMapping("/feed/{club_id}/list")
-    public List<FeedDTO.getFeedClub> getFeedClubList(@RequestParam("page") int page, @PathVariable("club_id") int club_id){
+    public List<GetFeedClub> getFeedClubList(@RequestParam("page") int page, @PathVariable("club_id") int club_id){
         if(page < 0){
             throw new BadRequestException();
         }
@@ -49,37 +47,37 @@ public class FeedController {
     }
 
     @PutMapping("/feed/{feed_id}")
-    public FeedDTO.messageResponse feedModify(@RequestBody FeedDTO.feed request, @PathVariable("feed_id") int feedId){
+    public MessageResponse feedModify(@RequestBody Feed request, @PathVariable("feed_id") int feedId){
         return feedService.feedModify(request, feedId);
     }
 
     @PutMapping("/feed/{feed_id}/flag")
-    public FeedDTO.messageResponse feedFlag(@PathVariable("feed_id") int feedId){
+    public MessageResponse feedFlag(@PathVariable("feed_id") int feedId){
         return feedService.feedFlag(feedId);
     }
 
     @GetMapping("/feed/{feed_id}")
-    public FeedDTO.getFeed getFeed(@PathVariable("feed_id") int feed_id){
+    public GetFeed getFeed(@PathVariable("feed_id") int feed_id){
         return feedService.getFeed(feed_id);
     }
 
     @DeleteMapping("/feed/{feed_id}")
-    public FeedDTO.messageResponse deleteFeed(@PathVariable("feed_id") int feed_id){
+    public MessageResponse deleteFeed(@PathVariable("feed_id") int feed_id){
         return feedService.deleteFeed(feed_id);
     }
 
     @PutMapping("/feed/{feed_id}/pin")
-    public FeedDTO.messageResponse feedPin(@PathVariable("feed_id") int feedId){
+    public MessageResponse feedPin(@PathVariable("feed_id") int feedId){
         return feedService.feedPin(feedId);
     }
 
     @GetMapping("/feed/flag")
-    public List<FeedDTO.getFeed> getFlagFeed(){
+    public List<GetFeed> getFlagFeed(){
         return feedService.getFeedList();
     }
 
     @GetMapping("/feed/{feed_id}/user")
-    public List<FeedDTO.userResponse> getFlagUser(@PathVariable("feed_id") int feedId){
+    public List<UserResponse> getFlagUser(@PathVariable("feed_id") int feedId){
         return feedService.getFeedUser(feedId);
     }
 
