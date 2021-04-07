47c68477c9816df39529a6208711f2ca32a70235
leeseojune <sung07288346@gmail.com>
2021-04-07 17:21:50 +0900

    UPDATE; ClubController

--- src/main/java/com/semicolon/spring/controller/ClubController.java
+++ src/main/java/com/semicolon/spring/controller/ClubController.java
@@ -1,6 +1,6 @@
 package com.semicolon.spring.controller;
 
-import com.semicolon.spring.dto.ClubDTO;
+import com.semicolon.spring.dto.ClubDTO.*;
 import com.semicolon.spring.service.club.ClubService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +15,12 @@ public class ClubController {
     private final ClubService clubService;
 
     @GetMapping("/{club_id}/follow")
-    public ClubDTO.followers getFollowers(@PathVariable("club_id") int club_id){
+    public Followers getFollowers(@PathVariable("club_id") int club_id){
         return clubService.getFollowers(club_id);
     }
 
     @GetMapping("/follow")
-    public List<ClubDTO.club> getClubs(){
+    public List<Club> getClubs(){
         return clubService.getClubs();
     }
 }
