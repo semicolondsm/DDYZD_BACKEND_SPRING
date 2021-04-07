e31f6f8532a13fd86183a9f7ee7f8f7efeb37046
leeseojune <sung07288346@gmail.com>
2021-04-07 17:06:44 +0900

    UPDATE; ClubHeadService

--- src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
+++ src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
@@ -6,4 +6,6 @@ public interface ClubHeadService {
     ClubDTO.messageResponse deportMember(int club_id, int user_id);
     ClubDTO.messageResponse insertMember(int club_id, int user_id);
     ClubDTO.messageResponse changeHead(ClubDTO.changeHead request, int club_id);
+    ClubDTO.messageResponse insertManager(int club_id, int user_id);
+    ClubDTO.messageResponse deportManager(int club_id, int user_id);
 }
