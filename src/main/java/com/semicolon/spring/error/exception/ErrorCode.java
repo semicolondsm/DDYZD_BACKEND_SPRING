9b9e148309945ec5e932d1b7e72030187085ccb9
leeseojune <sung07288346@gmail.com>
2021-04-07 17:06:31 +0900

    UPDATE; ErrorCode

--- src/main/java/com/semicolon/spring/error/exception/ErrorCode.java
+++ src/main/java/com/semicolon/spring/error/exception/ErrorCode.java
@@ -23,20 +23,21 @@ public enum ErrorCode {
     //File
     FILE_SAVE_FAIL(400, "File Save Fail."),
     FILE_NOT_FOUND(404, "File Not Fail."),
-    BAD_FILE_EXTENSION(400, "Bad File Extension"),
+    BAD_FILE_EXTENSION(400, "Bad File Extension."),
 
     //Club
     BAD_RECRUITMENT_TIME(400, "Bad Recruitment Time."),
-    APPLICATION_NOT_FOUND(400, "Application Not Found"),
-    DONT_KICK_YOUR_SELF(403, "1393"),
-    ALREADY_CLUB_MEMBER(400, "Already Club Member"),
+    APPLICATION_NOT_FOUND(400, "Application Not Found."),
+    DONT_KICK_YOUR_SELF(403, "call to 1393"),
+    ALREADY_CLUB_MEMBER(400, "Already Club Member,"),
+    ALREADY_CLUB_MANAGER(400, "Already Club Manager."),
     CLUB_NOT_FOUND(404, "Club Not Found."),
     NOT_CLUB_MEMBER(403, "Not Club Member."),
     NOT_CLUB_HEAD(403, "Not Club Head."),
     NOT_CLUB_MANAGER(403, "Not Club Manager."),
 
     //Erp
-    BAD_SUPPLY_LINK(400, "Bad Supply Link");
+    BAD_SUPPLY_LINK(400, "Bad Supply Link.");
 
     private final int status;
     private final String message;
