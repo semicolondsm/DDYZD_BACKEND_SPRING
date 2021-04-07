75da76392487075c71dea8e587ddea856fd7c7ee
leeseojune <sung07288346@gmail.com>
2021-04-07 17:21:44 +0900

    UPDATE; ApplicationController

--- src/main/java/com/semicolon/spring/controller/ApplicationController.java
+++ src/main/java/com/semicolon/spring/controller/ApplicationController.java
@@ -1,6 +1,6 @@
 package com.semicolon.spring.controller;
 
-import com.semicolon.spring.dto.HeadDTO;
+import com.semicolon.spring.dto.HeadDTO.*;
 import com.semicolon.spring.service.application.ApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +15,12 @@ public class ApplicationController {
     private final ApplicationService applicationService;
 
     @DeleteMapping("/cancel")
-    public HeadDTO.MessageResponse cancelApplication(@RequestParam("clubid") int club_id) throws ExecutionException, InterruptedException {
+    public MessageResponse cancelApplication(@RequestParam("clubid") int club_id) throws ExecutionException, InterruptedException {
         return applicationService.cancelApplication(club_id);
     }
 
     @DeleteMapping("/remove")
-    public HeadDTO.MessageResponse removeApplication(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id) throws ExecutionException, InterruptedException {
+    public MessageResponse removeApplication(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id) throws ExecutionException, InterruptedException {
         return applicationService.removeApplication(club_id, user_id);
     }
 
