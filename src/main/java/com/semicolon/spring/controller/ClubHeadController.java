01fa3a44fc463ff2016508a5f2b4e69cb825a17d
leeseojune <sung07288346@gmail.com>
2021-04-07 17:21:58 +0900

    UPDATE; ClubHeadController

--- src/main/java/com/semicolon/spring/controller/ClubHeadController.java
+++ src/main/java/com/semicolon/spring/controller/ClubHeadController.java
@@ -1,6 +1,6 @@
 package com.semicolon.spring.controller;
 
-import com.semicolon.spring.dto.ClubDTO;
+import com.semicolon.spring.dto.ClubDTO.*;
 import com.semicolon.spring.service.club_head.ClubHeadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,18 +13,28 @@ public class ClubHeadController {
     private final ClubHeadService clubHeadService;
 
     @PutMapping("/{club_id}/head")
-    public ClubDTO.messageResponse changeHead(@RequestBody ClubDTO.changeHead request, @PathVariable("club_id") int club_id){
+    public MessageResponse changeHead(@RequestBody ChangeHead request, @PathVariable("club_id") int club_id){
         return clubHeadService.changeHead(request, club_id);
     }
 
     @DeleteMapping("/deport")
-    public ClubDTO.messageResponse deportApplication(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id){
+    public MessageResponse deportMember(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id){
         return clubHeadService.deportMember(club_id, user_id);
     }
 
     @PostMapping("/insert")
-    public ClubDTO.messageResponse insertMember(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id) {
+    public MessageResponse insertMember(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id) {
         return clubHeadService.insertMember(club_id, user_id);
+    }
+
+    @PostMapping("/insert/manager")
+    public MessageResponse insertManager(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id){
+        return clubHeadService.insertManager(club_id, user_id);
+    }
+
+    @DeleteMapping("/deport/manager")
+    public MessageResponse deportManager(@RequestParam("clubid") int club_id, @RequestParam("userid") int user_id){
+        return clubHeadService.deportManager(club_id, user_id);
     }
 
 }
