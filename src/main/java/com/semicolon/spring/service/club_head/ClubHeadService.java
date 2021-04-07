59be3f6fe2957657db89ac8d17211d5aec53b6ed
leeseojune <sung07288346@gmail.com>
2021-04-07 16:57:00 +0900

    CREATE; ClubHeadService

---
+++ src/main/java/com/semicolon/spring/service/club_head/ClubHeadService.java
@@ -0,0 +1,9 @@
+package com.semicolon.spring.service.club_head;
+
+import com.semicolon.spring.dto.ClubDTO;
+
+public interface ClubHeadService {
+    ClubDTO.messageResponse deportMember(int club_id, int user_id);
+    ClubDTO.messageResponse insertMember(int club_id, int user_id);
+    ClubDTO.messageResponse changeHead(ClubDTO.changeHead request, int club_id);
+}
