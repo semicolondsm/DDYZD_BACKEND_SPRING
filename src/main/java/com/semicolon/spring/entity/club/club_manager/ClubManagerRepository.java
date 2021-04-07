8b9b034c83ee87a736fa8e6fb3901a1d0a3c2df9
leeseojune <sung07288346@gmail.com>
2021-04-07 17:06:24 +0900

    UPDATE; ClubManagerRepository

--- src/main/java/com/semicolon/spring/entity/club/club_manager/ClubManagerRepository.java
+++ src/main/java/com/semicolon/spring/entity/club/club_manager/ClubManagerRepository.java
@@ -4,8 +4,11 @@ import com.semicolon.spring.entity.club.Club;
 import com.semicolon.spring.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface ClubManagerRepository extends CrudRepository<ClubManager, Integer> {
     Optional<ClubManager> findByClubAndUser(Club club, User user);
+    @Transactional
+    void deleteByClubAndUser(Club club, User user);
 }
