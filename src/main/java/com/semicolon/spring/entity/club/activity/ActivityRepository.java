2b0226201938ecd66a7ce8369eb7988dce183e87
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-26 19:58:27 +0900

    DELETE; Activity, ActivityRepository

--- src/main/java/com/semicolon/spring/entity/club/activity/ActivityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.semicolon.spring.entity.club.activity;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ActivityRepository extends CrudRepository<Activity, Integer> {
-}
