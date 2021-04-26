7569018792e9d2655df656c05555adb64cffa09b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-26 18:51:25 +0900

    CREATE; ActivityRepository

---
+++ src/main/java/com/semicolon/spring/entity/club/activity/ActivityRepository.java
@@ -0,0 +1,6 @@
+package com.semicolon.spring.entity.club.activity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActivityRepository extends CrudRepository<Activity, Integer> {
+}
