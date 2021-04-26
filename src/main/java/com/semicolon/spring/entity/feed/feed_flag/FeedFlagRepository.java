eab6ac0a6e25afb16bdf7c58d9310eccada72534
leeseojune <sung07288346@gmail.com>
2021-04-26 19:23:41 +0900

    UPDATE; FeedFlagRepository

--- src/main/java/com/semicolon/spring/entity/feed/feed_flag/FeedFlagRepository.java
+++ src/main/java/com/semicolon/spring/entity/feed/feed_flag/FeedFlagRepository.java
@@ -2,13 +2,17 @@ package com.semicolon.spring.entity.feed.feed_flag;
 
 import com.semicolon.spring.entity.feed.Feed;
 import com.semicolon.spring.entity.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FeedFlagRepository extends CrudRepository<FeedFlag, Integer> {
     Optional<FeedFlag> findByUserAndFeed(User User, Feed Feed);
+    Page<FeedFlag> findByUser(User user, Pageable pageable);
     int countByFeed(Feed Feed);
 }
