8768d1954c7e89d5bcd8c2a121dcd265682dfa4a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-26 19:34:34 +0900

    CREATE; ActivityDetailRepository

---
+++ src/main/java/com/semicolon/spring/entity/club/activity_detail/ActivityDetailRepository.java
@@ -0,0 +1,6 @@
+package com.semicolon.spring.entity.club.activity_detail;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActivityDetailRepository extends CrudRepository<ActivityDetail, Integer> {
+}
