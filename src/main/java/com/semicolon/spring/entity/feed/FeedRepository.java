90689d27d5228aa712a684a9384bbc1c7bbbb292
leeseojune <sung07288346@gmail.com>
2021-04-27 15:31:47 +0900

    UPDATE; FeedRepository

--- src/main/java/com/semicolon/spring/entity/feed/FeedRepository.java
+++ src/main/java/com/semicolon/spring/entity/feed/FeedRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
 
 
