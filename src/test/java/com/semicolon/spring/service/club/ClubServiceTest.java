128ebc1858c0dd6404b08fa2e93e2d09cb88ac14
smoothbear <kjbin0420@naver.com>
2021-04-30 17:25:32 +0900

    UPDATE; Unused imports
    사용하지 않는 import 구문 삭제

--- src/test/java/com/semicolon/spring/service/club/ClubServiceTest.java
+++ src/test/java/com/semicolon/spring/service/club/ClubServiceTest.java
@@ -11,13 +11,10 @@ import com.semicolon.spring.security.AuthenticationFacade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
