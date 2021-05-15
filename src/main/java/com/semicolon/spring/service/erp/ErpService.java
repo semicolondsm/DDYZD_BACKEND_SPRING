353f86c7d2ca7c151c60e757b5c46b24038fe694
leeseojune <sung07288346@gmail.com>
2021-05-15 17:48:32 +0900

    UPDATE; CodeSmell ErpService

--- src/main/java/com/semicolon/spring/service/erp/ErpService.java
+++ src/main/java/com/semicolon/spring/service/erp/ErpService.java
@@ -3,7 +3,6 @@ package com.semicolon.spring.service.erp;
 import com.semicolon.spring.dto.ErpDTO;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 
 public interface ErpService {
     ErpDTO.Supply supply(ErpDTO.Url url) throws IOException;
