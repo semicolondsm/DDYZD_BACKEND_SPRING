972271a924a7ac96b0b376276087de63ec581d50
leeseojune <sung07288346@gmail.com>
2021-05-15 17:47:43 +0900

    UPDATE; CodeSmell ApplicationService

--- src/main/java/com/semicolon/spring/service/application/ApplicationService.java
+++ src/main/java/com/semicolon/spring/service/application/ApplicationService.java
@@ -5,6 +5,6 @@ import com.semicolon.spring.dto.HeadDTO;
 import java.util.concurrent.ExecutionException;
 
 public interface ApplicationService {
-    HeadDTO.MessageResponse cancelApplication(int club_id) throws ExecutionException, InterruptedException;
-    HeadDTO.MessageResponse removeApplication(int club_id, int user_id) throws ExecutionException, InterruptedException;
+    HeadDTO.MessageResponse cancelApplication(int clubId) throws ExecutionException, InterruptedException;
+    HeadDTO.MessageResponse removeApplication(int clubId, int userId) throws ExecutionException, InterruptedException;
 }
