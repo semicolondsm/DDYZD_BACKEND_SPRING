3b7e1f13e6be2240c869107f1976926f034724fa
leeseojune <sung07288346@gmail.com>
2021-05-15 17:44:29 +0900

    UPDATE; CodeSmell FeedDTO

--- src/main/java/com/semicolon/spring/dto/FeedDTO.java
+++ src/main/java/com/semicolon/spring/dto/FeedDTO.java
@@ -1,7 +1,6 @@
 package com.semicolon.spring.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
