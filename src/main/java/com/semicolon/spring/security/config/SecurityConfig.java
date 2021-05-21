57e7cc29f87163087cbdcdffa8d301b614714778
leeseojune <sung07288346@gmail.com>
2021-05-21 10:20:46 +0900

    UPDATE; SecurityConfig swagger

--- src/main/java/com/semicolon/spring/security/config/SecurityConfig.java
+++ src/main/java/com/semicolon/spring/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -53,5 +54,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/**");
     }
 }
