96c181fbb65b302981d429f9db2749754deceb25
leeseojune <sung07288346@gmail.com>
2021-09-02 20:08:53 +0900

    ♻ :: 반환형 수정

--- src/main/java/com/semicolon/spring/controller/FeedController.java
+++ src/main/java/com/semicolon/spring/controller/FeedController.java
@@ -1,6 +1,9 @@
 package com.semicolon.spring.controller;
 
-import com.semicolon.spring.dto.FeedDTO.*;
+import com.semicolon.spring.dto.feed.request.ContentRequest;
+import com.semicolon.spring.dto.feed.response.GetFeedClubResponse;
+import com.semicolon.spring.dto.feed.response.GetFeedResponse;
+import com.semicolon.spring.dto.user.response.UserInfoResponse;
 import com.semicolon.spring.exception.BadRequestException;
 import com.semicolon.spring.service.feed.FeedService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 
 import java.util.List;
 
@@ -20,18 +22,18 @@ public class FeedController {
 
     @PostMapping("/feed/{feedId}/medium")
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponse fileUpload(@RequestParam("files") List<MultipartFile> files, @PathVariable("feedId") int feedId){
-        return feedService.fileUpload(files, feedId);
+    public void fileUpload(@RequestParam("files") List<MultipartFile> files, @PathVariable("feedId") int feedId){
+        feedService.fileUpload(files, feedId);
     }
 
     @PostMapping("/feed/{club_id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public WriteFeedResponse feedUpload(@RequestBody Feed request, @PathVariable("club_id") int clubId){
-        return feedService.writeFeed(request, clubId);
+    public void feedUpload(@RequestBody ContentRequest request, @PathVariable("club_id") int clubId){
+        feedService.writeFeed(request, clubId);
     }
 
     @GetMapping("/feed/list")
-    public List<GetFeed> getFeedList(@RequestParam("page") int page){
+    public List<GetFeedResponse> getFeedList(@RequestParam("page") int page){
         if(page < 0){
             throw new BadRequestException();
         }
@@ -39,7 +41,7 @@ public class FeedController {
     }
 
     @GetMapping("/feed/{club_id}/list")
-    public List<GetFeedClub> getFeedClubList(@RequestParam("page") int page, @PathVariable("club_id") int clubId){
+    public List<GetFeedClubResponse> getFeedClubList(@RequestParam("page") int page, @PathVariable("club_id") int clubId){
         if(page < 0){
             throw new BadRequestException();
         }
@@ -47,32 +49,36 @@ public class FeedController {
     }
 
     @PutMapping("/feed/{feed_id}")
-    public MessageResponse feedModify(@RequestBody Feed request, @PathVariable("feed_id") int feedId){
-        return feedService.feedModify(request, feedId);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void feedModify(@RequestBody ContentRequest request, @PathVariable("feed_id") int feedId){
+        feedService.feedModify(request, feedId);
     }
 
     @PutMapping("/feed/{feed_id}/flag")
-    public MessageResponse feedFlag(@PathVariable("feed_id") int feedId){
-        return feedService.feedFlag(feedId);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void feedFlag(@PathVariable("feed_id") int feedId){
+        feedService.feedFlag(feedId);
     }
 
     @GetMapping("/feed/{feed_id}")
-    public GetFeed getFeed(@PathVariable("feed_id") int feedId){
+    public GetFeedResponse getFeed(@PathVariable("feed_id") int feedId){
         return feedService.getFeed(feedId);
     }
 
     @DeleteMapping("/feed/{feed_id}")
-    public MessageResponse deleteFeed(@PathVariable("feed_id") int feedId){
-        return feedService.deleteFeed(feedId);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFeed(@PathVariable("feed_id") int feedId){
+        feedService.deleteFeed(feedId);
     }
 
     @PutMapping("/feed/{feed_id}/pin")
-    public MessageResponse feedPin(@PathVariable("feed_id") int feedId){
-        return feedService.feedPin(feedId);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void feedPin(@PathVariable("feed_id") int feedId){
+        feedService.feedPin(feedId);
     }
 
     @GetMapping("/feed/flag")
-    public List<GetFeed> getFlagFeed(@RequestParam("page") int page){
+    public List<GetFeedResponse> getFlagFeed(@RequestParam("page") int page){
         if(page < 0){
             throw new BadRequestException();
         }
@@ -80,7 +86,7 @@ public class FeedController {
     }
 
     @GetMapping("/feed/{feed_id}/user")
-    public List<UserResponse> getFlagUser(@PathVariable("feed_id") int feedId){
+    public List<UserInfoResponse> getFlagUser(@PathVariable("feed_id") int feedId){
         return feedService.getFeedUser(feedId);
     }
 
