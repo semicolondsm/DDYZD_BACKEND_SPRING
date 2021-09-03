9a4ef4e8af31243b96e527bc6144dc9d78d228f7
leeseojune <sung07288346@gmail.com>
2021-09-04 01:54:52 +0900

    📑 :: HongboResponse

---
+++ src/main/java/com/semicolon/spring/dto/club/response/HongboResponse.java
@@ -0,0 +1,12 @@
+package com.semicolon.spring.dto.club.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class HongboResponse {
+
+    private String hongbo;
+
+}
