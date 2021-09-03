8e6ac071f99731052e4aa1449e7b6548a1551573
leeseojune <sung07288346@gmail.com>
2021-09-04 01:53:58 +0900

    📑 :: DescriptionRequest

---
+++ src/main/java/com/semicolon/spring/dto/club/request/DescriptionRequest.java
@@ -0,0 +1,12 @@
+package com.semicolon.spring.dto.club.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DescriptionRequest {
+
+    private String description;
+
+}
