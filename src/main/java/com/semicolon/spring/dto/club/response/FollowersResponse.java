3a22736fb712a2fa3f21ab6eef4865eda46e66c1
leeseojune <sung07288346@gmail.com>
2021-09-04 01:54:42 +0900

    📑 :: FollowersResponse

---
+++ src/main/java/com/semicolon/spring/dto/club/response/FollowersResponse.java
@@ -0,0 +1,12 @@
+package com.semicolon.spring.dto.club.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FollowersResponse {
+
+    private int followers;
+
+}
